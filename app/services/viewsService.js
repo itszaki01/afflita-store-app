5f80ae652157096a1e7b3e0cd5d796b7f38bb1a8
Blx <itszacktouil@gmail.com>
2024-02-08 14:08:40 +0100

    fixed

--- app/services/viewsService.js
+++ app/services/viewsService.js
@@ -86,7 +86,7 @@ exports.homeView = (0, express_async_handler_1.default)(async (req, res) => {
                     s.parentNode.insertBefore(t, s);
                 })(window, document, "script", "https://connect.facebook.net/en_US/fbevents.js");
             </script>
-            <script type="module" crossorigin src="/assets/index-WITH6Pj7.js"></script>
+            <script type="module" crossorigin src="/assets/index-jXmp5KzX.js"></script>
             <link rel="stylesheet" crossorigin href="/assets/index-IZka7ouJ.css">
         </head>
         <body>
